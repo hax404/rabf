594f9dbc668928d4ac346d41df7b4d0d749073d2
Georg Haas <hax404foogit@hax404.de>
2015-05-14 18:44:25 +0200

    add generation of address

--- addr.py
+++ addr.py
@@ -4,25 +4,37 @@ import hashlib
 
 parser = argparse.ArgumentParser(description='Generate and verify Address')
 parser.add_argument('--verify')
+parser.add_argument('--generate')
 args = parser.parse_args()
 
 def verify_addr(addr):
   euser = addr[0:2]
   edomain = addr.split("@")[1]
   ecsum = addr[2:4]
-  eitext = ''.join((euser, addr.split("@")[0][4:]))
-  gencsum = hashlib.sha512(eitext.encode('utf-8')).hexdigest()[41:43]
-
-  print(euser, edomain, ecsum, eitext, gencsum)
-  print(hashlib.sha512(eitext.encode('utf-8')).hexdigest())
+  eitext = addr.split("@")[0][4:]
+  genitext = ''.join((euser, eitext))
+  gencsum = hashlib.sha512(genitext.encode('utf-8')).hexdigest()[41:43]
 
   if (ecsum == gencsum):
     print ("Checksum of address", addr, "is right")
   else:
     print ("Checksums differ:\nextracted Checksum:", ecsum, "\ngenerated Checksum:", gencsum)
 
+def gen_addr(addr):
+  euser = addr[0:2]
+  edomain = addr.split("@")[1]
+  eitext = addr.split("@")[0][2:]
+  genitext = ''.join((euser, eitext))
+  gencsum = hashlib.sha512(genitext.encode('utf-8')).hexdigest()[41:43]
+
+  print ("Generated Checksum:", gencsum)
+  print ("Generated Address:", "".join((euser,gencsum,eitext,"@",edomain)))
+
 def main():
-  verify_addr(args.verify)
+  if args.verify:
+    verify_addr(args.verify)
+  elif args.generate:
+    gen_addr(args.generate)
 
 if __name__ == "__main__":
   main()
